7ddd3ce221dced25c9b15cadc74b515fd678948b
Ogehenetega umurhurhu <tegaumurhurhu@gmail.com>
2024-04-19 12:07:49 +0100

    Successfull
    Completed the minor task for he account controller.

---
+++ ShoppingApiProject/ShoppingApi/SMTP/EmailSender.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace ShoppingApi.SMTP
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var client = new SmtpClient("smtp.office365.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("tegaumurhurhu@gmail.com", "zviukwzyvuhwsoas")
+            };
+            client.EnableSsl = true;
+
+            return client.SendMailAsync(
+                new MailMessage(from: "your.email@live.com",
+                                to: email,
+                                subject,
+                                message
+                                ));
+        }
+    }
+}
